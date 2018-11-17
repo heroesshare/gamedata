5919ac7515bb6140de2dd7c4d833ae8f28b0910d
MGatner <mgatner@tattersoftware.com>
2018-11-17 12:11:59 -0500

    add image compression

--- extract.sh
+++ extract.sh
@@ -51,6 +51,21 @@ if [ ! -d "$hotsDir" ]; then
 	exit 1
 fi
 
+# programs for image conversion
+magickPath=`which magick`
+if [ ! "$magickPath" ]; then
+	echo "[`date`] ERROR: Unmet requirement: missing dependency 'magick'"
+	echo "[`date`] Recommend 'brew install ImageMagick'"
+	exit 1
+fi
+
+renamePath=`which rename`
+if [ ! "$magickPath" ]; then
+	echo "[`date`] ERROR: Unmet requirement: missing dependency 'rename'"
+	echo "[`date`] Recommend 'brew install rename'"
+	exit 1
+fi
+
 
 ### REPO UPDATES ###
 
@@ -59,8 +74,8 @@ cd "$selfDir"
 git remote update
 repoStatus=`git status -uno | grep behind`
 if [ "$repoStatus" ]; then
-	echo "[`date`] Self out of date! Updating and quitting... "
 	git pull
+	echo "[`date`] Self out of date! Updated - please rerun. "
 	exit 0
 else
 	echo "[`date`] Self is current, proceeding."
@@ -72,10 +87,11 @@ git remote update
 repoStatus=`git status -uno | grep behind`
 if [ "$repoStatus" ]; then
 	echo "[`date`] HeroesDataParser out of date! Updating... "
+	git pull
+	
 	echo "[`date`] Please download the corresponding release into ~/Library:"
 	echo "[`date`] https://github.com/koliva8245/HeroesDataParser/releases"
 
-	git pull
 	exit 0
 else
 	echo "[`date`] HeroesDataParser is current, proceeding."
@@ -114,11 +130,17 @@ if [ $? -ne 0 ]; then
 	read -p "[`date`] Press enter to continue, Ctrl+C to abort"
 fi
 
+# show diff of extracted JSON data
+diff "$extractDir/raw/json/heroesdata_*_enus.json" "$tmpDir/json/heroesdata_*_enus.json" | less
+
+# wait for approval
+echo "[`date`] Ready to copy extracted data to $extractDir/raw"
+read -p "[`date`] Press enter to continue, Ctrl+C to abort"
+
 # remove old versions
 rm -rf "$extractDir/raw"
 mkdir "$extractDir/raw"
 
-echo "[`date`] Copy extracted data to $extractDir/raw"
 cp -R $tmpDir/* "$extractDir/raw/"
 
 # verify move
@@ -126,6 +148,27 @@ if [ $? -ne 0 ]; then
 	echo "[`date`] Move seems to have failed!"
 	read -p "[`date`] Press enter to continue, Ctrl+C to abort"
 fi
+
+
+### IMAGE RESIZE
+
+echo "[`date`] Resizing talent icons to 64x64"
+cd "$tmpdir/images/abilityTalents"
+"$magickPath" mogrify -resize 64x64 *.png
+
+# verify status
+if [ $? -ne 0 ]; then
+	echo "[`date`] Image resize seems to have failed!"
+	read -p "[`date`] Press enter to continue, Ctrl+C to abort"
+fi
+
+# remove apostrophes from filenames (mostly Kel'Thuzad)
+"$renamePath" "s/'//" *.png
+
+# remove old icons
+#rm -rf "$extractDir/images/talents"
+# copy in new
+cp "$tmpdir/images/abilityTalents/*.png" "$extractDir/images/talents/"
 
 
 ### REPO COMMIT ###
